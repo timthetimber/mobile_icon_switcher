00be734d930f5877016e47646848032a7db1d805
Tim Hönlinger <kragsteine_00_zen@icloud.com>
2023-06-27 21:05:57 +0200

    Added tests

---
+++ test/icon_switcher_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:icon_switcher/icon_switcher.dart';
+
+class MockMethodChannel extends Mock implements MethodChannel {}
+
+void main() {
+  const testChannel = MethodChannel('app_icon_switcher');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
+
+    TestDefaultBinaryMessengerBinding.instance?.defaultBinaryMessenger
+        .setMockMethodCallHandler(testChannel, (call) async {
+      switch (call.method) {
+        case 'getPlatformVersion':
+          return '1.0.0';
+        case 'changeIcon':
+          return true;
+        case 'defaultComponent':
+          return true;
+        default:
+          return null;
+      }
+    });
+  });
+
+  tearDown(() {
+    testChannel.setMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await IconSwitcher.platformVersion, '1.0.0');
+  });
+
+  test('changeIcon', () async {
+    expect(await IconSwitcher.changeIcon('icon1', 'alias1'), true);
+  });
+
+  test('setDefaultComponent', () async {
+    expect(
+        await IconSwitcher.setDefaultComponent(
+            'com.example.example.MainActivity'),
+        true);
+  });
+}
